77d1461b34ddd11558ff3ea0dbd75099f7216fa1
Bastardboy <david.pazan2@gmail.com>
2022-11-25 20:57:51 -0300

    xd

--- examples/mapper.py
+++ examples/mapper.py
@@ -6,13 +6,9 @@ import re
 fi = 0
 for line in sys.stdin:
     l = line.lower()
-    if("xdxdxd" in l):
-        fi = int(l[0])
-    else:
-        for char in [",", ".", '"', "'", "(", ")", "\\", ";", ":", "$1", "$", "&"]:
-            l = l.replace(char, '')
+    for char in [",", ".", '"', "'", "(", ")", "\\", ";", ":", "$1", "$", "&"]:
+        l = l.replace(char, '')
             
-        word = l.split()
-
-        for words in enumerate(word):
-            print('{}\t{}'.format(words[1], fi))
+    word = l.split()
+    for words in enumerate(word):
+        print('{}\t{}'.format(words[1], word[0]))
